3475ed4a8cf44c5c411454ccd141c3a31d7753a0
ElTricksterJack <a.23308051281212@cbtis128.edu.mx>
2024-11-24 13:18:36 -0700

    Update Punto 2.py

--- Punto 2.py
+++ Punto 2.py
@@ -7,7 +7,9 @@ while c < 1:#este while te permite ingresar un numero de veces que quieras.
     nu.append(n)#se podria desir que eso permite agregar un elemento a la lista como nuevos valores sin coregir los otros.
     c = int(input("deseas alladir otro numero? 1 si 0 no:"))
 print("Lista:",nu)#mostramos la lista
-may=max(nu, key=len)#ahora la palabra mas larga buscando entre la lista ya que el len cuenta los elementos 1 por uno y el max saca el maximo
-print("la palabra mas larga es:",may)#ahora solo se muestra el mayor
+def may(n):
+    may=max(n, key=len)#ahora la palabra mas larga buscando entre la lista ya que el len cuenta los elementos 1 por uno y el max saca el maximo
+    print("la palabra mas larga es:",may)
+may(nu)
 print("-------------------------------------")
 print("Resultado: se puede crear un histograma fasilente.\n")
