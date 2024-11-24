368326af126b5c791ac8027b60caf3d26012bf8f
ElTricksterJack <a.23308051281212@cbtis128.edu.mx>
2024-11-24 16:52:24 -0700

    Create Punto 6.py

---
+++ Punto 6.py
@@ -0,0 +1,21 @@
+print("\nRuvalcaba Valverde Miguel Angel_1212_3W")
+print("---------------Punto 6---------------")
+a = int(input("que año es: "))
+n1 = input("Escribe un nombre: ")
+n2 = input("Escribe otro nombre: ")
+n3 = input("Escribe un ultimo nombre: ")
+#hay porfin
+na1 = int(input(f"Ahora escribe el año en el que nacion {n1}: "))
+#AQUE???¡¡¡
+na2 = int(input(f"Ahora escribe el año en el que nacion {n2}: "))
+na3 = int(input(f"Ahora escribe el año en el que nacion {n3}: "))
+#a bueno entoses ya verdad?
+#Miguel: si
+f1 = a - na1
+f2 = a - na2
+f3 = a - na3
+print(f"Actual a la fecha {a} {n1} tiene {f1} años")
+print(f"Actual a la fecha {a} {n2} tiene {f2} años")
+print(f"Actual a la fecha {a} {n3} tiene {f3} años")
+#operaciones simples, bueno esto fue algo relajante
+print("-------------------------------------\n")
