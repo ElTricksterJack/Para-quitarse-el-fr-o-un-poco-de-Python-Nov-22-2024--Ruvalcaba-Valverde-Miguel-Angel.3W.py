5fd7af657ee112dd68a98541dc855bd7443657ab
ElTricksterJack <a.23308051281212@cbtis128.edu.mx>
2024-11-24 13:08:35 -0700

    Create Punto 2.py

---
+++ Punto 2.py
@@ -0,0 +1,13 @@
+print("\nRuvalcaba Valverde Miguel Angel_1212_3W")
+print("---------------Punto 2---------------")
+c = 0
+nu = []
+while c < 1:#este while te permite ingresar un numero de veces que quieras.
+    n = input("ingresa una palabra:")
+    nu.append(n)#se podria desir que eso permite agregar un elemento a la lista como nuevos valores sin coregir los otros.
+    c = int(input("deseas alladir otro numero? 1 si 0 no:"))
+print("Lista:",nu)#mostramos la lista
+may=max(nu, key=len)#ahora la palabra mas larga buscando entre la lista ya que el len cuenta los elementos 1 por uno y el max saca el maximo
+print("la palabra mas larga es:",may)#ahora solo se muestra el mayor
+print("-------------------------------------")
+print("Resultado: se puede crear un histograma fasilente.\n")
